8fc56c1bf438e2d36189f2196acff8c30f87a65a
FabioCampello <fabiocampello@outlook.com.br>
2020-03-08 21:03:14 -0300

    12 - Adicionados parâmetros de configuração e controller de versão.

---
+++ src/main/java/com/spring/tutorial/controller/VersaoController.java
@@ -0,0 +1,31 @@
+package com.spring.tutorial.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/versao")
+public class VersaoController {
+	
+	@Value("${application.versionAPI}")
+	private String versao;
+	
+	@Value("${paginacao.paginacao}")
+	private int paginacao;
+	
+	@GetMapping("/")
+	public ResponseEntity<String> recuperaVersao() {
+		return ResponseEntity.ok().body("Version API: " + versao);
+	}
+	
+	@GetMapping("/recuperaPaginacao")
+	public ResponseEntity<String> recuperaPaginacao() {
+		return ResponseEntity.ok().body("Paginação: " + paginacao);
+	} 
+
+}
